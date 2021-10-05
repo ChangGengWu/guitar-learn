3e0f6ce0cd108f62c083f11d4655eb9fed23b7cf
changgengwu <a8040333@gmail.com>
2021-10-05 14:12:01 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <YoutubePlayer />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import YoutubePlayer from "@/components/YoutubePlayer.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    YoutubePlayer,
+  },
+};
 </script>
